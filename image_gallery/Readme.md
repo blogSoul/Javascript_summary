b3cb2184c48e2bbf993b4b645ea14649afb2aecc
blogSoul <blo9040@gmail.com>
2020-04-18 23:14:32 +0900

    Add section49

--- image_gallery/Readme.md
+++ image_gallery/Readme.md
@@ -1,1 +1,15 @@
-# 갤러리 프로젝트입니다.
+#### section 50 이미지 갤러리 만들기
+
+context - padding - border - margin - position 순으로 생각하면 됩니다.
+padding과 border는 모두 width에 포함되지 않습니다.
+margin은 다른 태그와의 거리를 나타냅니다.
+border은 영역을 표현하는 선의 두께를 나타냅니다.
+padiing은 context 영역에서부터 얼마나 멀리 할 것인지 나타냅니다.
+
+*css 안에서 모르는 특성들
+
+z-index : 기본적으로 엘리먼트들이 기본적으로 어떻게 쌓이는지 설명합니다.
+opactiy : 요소의 투명도를 설정할 수 있습니다.
+
+#### section 50 이미지 선택 기능 만들기
+
--- image_gallery/script.js
+++ image_gallery/script.js
@@ -1,15 +1,19 @@
 var req = new XMLHttpRequest();// 객체 생성
-req.open("GET", "./json/image_list.json");// json형식 파일 불러오기
+req.open("GET", "./image_list.json");// json형식 파일 불러오기
 req.onreadystatechange = function() {
-  if (this.readyState == 4) {
+  if (this.readyState == 4) { // 모든 데이터가 정삭적으로 수신된 경우입니다.
   // console.log(this.response);
   var data = JSON.parse(this.response);
   for (var i = 0; i < data.length; i++) {
       var div = document.createElement("div");
       div.setAttribute("class", "image");
-      div.onclick = function(){
-         this.classList.toggle("image-selected")
-         //클릭을 하면 속성을 부여한다.
+      div.onclick = function() {
+         if(this.getAttribute("class").indexOf("image-selected") == -1){
+            this.setAttribute("class", "image image-selected");
+         }
+         else{
+            this.setAttribute("class", "image");
+         };
       };
       var img = document.createElement("img");
       img.src = data[i];
